7a327b84a6bb20ac5055b1c0c1219d92fde316ce
Aaron Swartz <me@aaronsw.com>
2011-01-28 15:05:17 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import sys
+from setuptools import setup, find_packages
+
+setup(
+   name = "html2text",
+   version = "2.39",
+   description = "Turn HTML into equivalent Markdown-structured text.",
+   author = "Aaron Swartz",
+   author_email = "me@aaronsw.com",
+   url='http://www.aaronsw.com/2002/html2text/',
+   classifiers=[
+       'Development Status :: 5 - Production/Stable',
+       'Intended Audience :: Developers',
+       'License :: OSI Approved :: GNU General Public License (GPL)',
+     ],
+   license='GNU GPL 3',
+   packages=find_packages(),
+   py_modules=['html2text'],
+   include_package_data=True,
+   zip_safe=False,
+)
