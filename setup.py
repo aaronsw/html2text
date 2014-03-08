b2c2bb7d718d70333f0e2c1b82f3007689c062bd
Alireza Savand <alireza.savand@gmail.com>
2014-03-08 19:13:44 +0400

    PEP8 on setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,35 @@
-import sys
 from setuptools import setup, find_packages
 
 setup(
-   name = "html2text",
-   version = "3.200.3",
-   description = "Turn HTML into equivalent Markdown-structured text.",
-   author = "Aaron Swartz",
-   author_email = "me@aaronsw.com",
-   url='http://www.aaronsw.com/2002/html2text/',
-   classifiers=[
-       'Development Status :: 5 - Production/Stable',
-       'Intended Audience :: Developers',
-       'License :: OSI Approved :: GNU General Public License (GPL)',
-       'Programming Language :: Python',
-       'Programming Language :: Python :: 2',
-       'Programming Language :: Python :: 2.3',
-       'Programming Language :: Python :: 2.4',
-       'Programming Language :: Python :: 2.5',
-       'Programming Language :: Python :: 2.6',
-       'Programming Language :: Python :: 2.7',
-       'Programming Language :: Python :: 3',
-       'Programming Language :: Python :: 3.0',
-       'Programming Language :: Python :: 3.1',
-       'Programming Language :: Python :: 3.2'
-     ],
+    name="html2text",
+    version="3.200.3",
+    description="Turn HTML into equivalent Markdown-structured text.",
+    author="Aaron Swartz",
+    author_email="me@aaronsw.com",
+    url='http://www.aaronsw.com/2002/html2text/',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2'
+    ],
     entry_points="""
         [console_scripts]
         html2text=html2text:main
     """,
-   license='GNU GPL 3',
-   packages=find_packages(),
-   py_modules=['html2text'],
-   include_package_data=True,
-   zip_safe=False,
+    license='GNU GPL 3',
+    packages=find_packages(),
+    py_modules=['html2text'],
+    include_package_data=True,
+    zip_safe=False,
 )
