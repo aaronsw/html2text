7bb3e60658248262a769c554ee420efc231481f8
Alireza Savand <alireza.savand@gmail.com>
2014-07-03 22:09:36 +0400

    Update version number

--- html2text.py
+++ html2text.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 """html2text: Turn HTML into equivalent Markdown-structured text."""
 from __future__ import division
-__version__ = "2014.4.5"
+__version__ = "2014.7.3"
 __author__ = "Aaron Swartz (me@aaronsw.com)"
 __copyright__ = "(C) 2004-2008 Aaron Swartz. GNU GPL 3."
 __contributors__ = ["Martin 'Joey' Schulze", "Ricardo Reyes",
