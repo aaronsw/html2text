02d126c0260692338ce9075e0711e0967c960f4e
Alireza Savand <alireza.savand@gmail.com>
2014-07-04 11:19:47 +0400

    Move const and conf vars to config.py module

--- html2text/__init__.py
+++ html2text/__init__.py
@@ -31,54 +31,11 @@ try:
 except ImportError:
     pass
 
-# Use Unicode characters instead of their ascii psuedo-replacements
-UNICODE_SNOB = 0
-
-# Escape all special characters.  Output is less readable, but avoids
-# corner case formatting issues.
-ESCAPE_SNOB = 0
-
-# Put the links after each paragraph instead of at the end.
-LINKS_EACH_PARAGRAPH = 0
-
-# Wrap long lines at position. 0 for no wrapping. (Requires Python 2.3.)
-BODY_WIDTH = 78
-
-# Don't show internal links (href="#local-anchor") -- corresponding link
-# targets won't be visible in the plain text file anyway.
-SKIP_INTERNAL_LINKS = True
-
-# Use inline, rather than reference, formatting for images and links
-INLINE_LINKS = True
-
-# Number of pixels Google indents nested lists
-GOOGLE_LIST_INDENT = 36
-
-IGNORE_ANCHORS = False
-IGNORE_IMAGES = False
-IGNORE_EMPHASIS = False
-
-### Entity Nonsense ###
-# For checking space-only lines on line 771
-SPACE_RE = re.compile(r'\s\+')
-
 
 def name2cp(k):
     if k == 'apos':
         return ord("'")
     return htmlentitydefs.name2codepoint[k]
-
-
-unifiable = {'rsquo': "'", 'lsquo': "'", 'rdquo': '"', 'ldquo': '"',
-             'copy': '(C)', 'mdash': '--', 'nbsp': ' ', 'rarr': '->',
-             'larr': '<-', 'middot': '*', 'ndash': '-', 'oelig': 'oe',
-             'aelig': 'ae', 'agrave': 'a', 'aacute': 'a', 'acirc': 'a',
-             'atilde': 'a', 'auml': 'a', 'aring': 'a', 'egrave': 'e',
-             'eacute': 'e', 'ecirc': 'e', 'euml': 'e', 'igrave': 'i',
-             'iacute': 'i', 'icirc': 'i', 'iuml': 'i', 'ograve': 'o',
-             'oacute': 'o', 'ocirc': 'o', 'otilde': 'o', 'ouml': 'o',
-             'ugrave': 'u', 'uacute': 'u', 'ucirc': 'u', 'uuml': 'u',
-             'lrm': '', 'rlm': ''}
 
 unifiable_n = {}
 
