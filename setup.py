da9d1904d20742bf97a744c545d361af802e1a58
Alireza Savand <alireza.savand@gmail.com>
2014-12-29 11:34:01 +0400

    Update version number for Release 2014.12.29

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class RunTests(Command):
 
 setup(
     name="html2text",
-    version="2014.12.24",
+    version="2014.12.29",
     description="Turn HTML into equivalent Markdown-structured text.",
     author="Aaron Swartz",
     author_email="me@aaronsw.com",
