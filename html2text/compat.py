9b07bb347f9eba8897970bcd1a8482a4c62f99be
Alireza Savand <alireza.savand@gmail.com>
2015-06-21 18:34:27 +0400

    try except block is slow

--- html2text/compat.py
+++ html2text/compat.py
@@ -1,12 +1,13 @@
-try:  # pragma: no cover
-    import urllib.parse as urlparse
-    import html.entities as htmlentitydefs
-    import html.parser as HTMLParser
-except ImportError:  # pragma: no cover
+import sys
+
+
+if sys.version_info[0] == 2:
     import htmlentitydefs
     import urlparse
     import HTMLParser
-try:  # pragma: no cover
-    import urllib.request as urllib
-except ImportError:  # pragma: no cover
     import urllib
+else:
+    import urllib.parse as urlparse
+    import html.entities as htmlentitydefs
+    import html.parser as HTMLParser
+    import urllib.request as urllib
