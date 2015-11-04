36be267c2dfa7bcef56fc30947e210cd1f63cd49
Alireza Savand <alireza.savand@gmail.com>
2015-11-04 14:47:26 +0000

    Add more python version to travis and setup.py classifiers

--- setup.py
+++ setup.py
@@ -58,7 +58,9 @@ setup(
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     entry_points="""
         [console_scripts]
