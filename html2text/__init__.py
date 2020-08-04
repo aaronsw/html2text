c18484a08aae0317e6d338b999563c4e8e4053d4
Edward Ross <edward@skeptric.com>
2020-08-04 23:54:43 +1000

    Append extra whitespace to preceding data when output.
    When an extra space is output for an emphasis tag then there is
    effectively an extra space in the preceding data.
    Append this space instead of clobbering the existing data.

--- html2text/__init__.py
+++ html2text/__init__.py
@@ -397,7 +397,7 @@ class HTML2Text(html.parser.HTMLParser):
         if tag in ["em", "i", "u"] and not self.ignore_emphasis:
             if start and no_preceding_space(self):
                 emphasis = " " + self.emphasis_mark
-                self.preceding_data = " "
+                self.preceding_data += " "
             else:
                 emphasis = self.emphasis_mark
 
@@ -408,7 +408,7 @@ class HTML2Text(html.parser.HTMLParser):
         if tag in ["strong", "b"] and not self.ignore_emphasis:
             if start and no_preceding_space(self):
                 strong = " " + self.strong_mark
-                self.preceding_data = " "
+                self.preceding_data += " "
             else:
                 strong = self.strong_mark
 
@@ -419,7 +419,7 @@ class HTML2Text(html.parser.HTMLParser):
         if tag in ["del", "strike", "s"]:
             if start and no_preceding_space(self):
                 strike = " ~~"
-                self.preceding_data = " "
+                self.preceding_data += " "
             else:
                 strike = "~~"
 
