bcc128859f12c0daacef28184dacd6885c4678d5
Maciek <podolskimaciej91@gmail.com>
2021-06-08 17:24:43 +0200

    flake8 lint

--- html2text/__init__.py
+++ html2text/__init__.py
@@ -682,7 +682,8 @@ class HTML2Text(html.parser.HTMLParser):
                             self.o("  \n")
                     else:
                         if self.pad_tables:
-                            self.soft_br() # add break in case the table is empty or its 1 row table
+                            # add break in case the table is empty or its 1 row table
+                            self.soft_br()
                             self.o("</" + config.TABLE_MARKER_FOR_PAD + ">")
                             self.o("  \n")
                 if tag in ["td", "th"] and start:
